b9d1ec334a9b9db395f660fdc8f2f30e2f630139
Marava1000 <mvillalbaalvarez@escuelasproa.edu.ar>
2024-08-15 09:31:39 -0300

    Add files via upload

---
+++ Programación6.py
@@ -0,0 +1,12 @@
+LadosDeFigura=int(input("ingrese cantidad de lados"))
+if lados==4:
+    l1=int(input("ingrese el valor del lado uno:"))
+    l2=int(input("ingrese el valor del lado dos:"))
+    l3=int(input("ingrese el valor del lado tres:"))
+    l4=int(input("ingrese el valos del lado cuatro:"))
+
+if l1==l2 and l1==l3 and l1==l4:
+    print("la figura puede ser un cuadrado o un rombo")
+elif l1!=l2 or l1!=3 or l1!=4:
+
+    
